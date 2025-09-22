446319e97165da470d63b27a962896817d48c73f
Jennifer Lima <limajennifer@gmail.com>
2025-09-22 07:58:42 -0400

    Criada pastas css e js

--- src/index.php
+++ src/index.php
@@ -23,7 +23,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FisioHub</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="img/teste.png">
 
 </head>
@@ -91,6 +91,6 @@ try {
             </section>
     </main>
 
-    <script src="script.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
